4ef9331ff3e65d856d749bd875907110cf4c1d21
Tanushree Chakravorty <tanushree998@gmail.com>
2023-03-08 21:06:40 +0530

    product line

--- Multiply.java
+++ Multiply.java
@@ -2,6 +2,6 @@ public class Multiply {
     public static void main(String[] args) 
     {
         int a=10;
-        System.out.println("Prod: "+(a*6));    
+        System.out.println("Product is : "+(a*6));    
     }
 }
